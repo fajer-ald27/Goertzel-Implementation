73a23a492b0371d499e49e03a725ecbac6b9abf8
BiniyamLombe <60434238+BiniyamLombe@users.noreply.github.com>
2021-06-12 15:06:01 +0100

    Update util.c

--- util.c
+++ util.c
@@ -32,11 +32,8 @@ short coef[8] = {0x6D02, 0x68AD, 0x63FC, 0x5EE7, 0x4A70, 0x4090, 0x3290, 0x23CE}
 
 void task1_dtmfDetect(void)
 {
-
-
 	int i, a1=0, a2=0, f1=0, f2=0, dig;
-
-
+	int input;
 
 	while (1) {
 
@@ -70,11 +67,48 @@ void task1_dtmfDetect(void)
 		Task_sleep(500);
 
 		// 2. Read the GTZ for each frequency and take a decision on which key has been pressed and print it.
-	    a1=0, a2=0, f1=0, f2=0;
+
+		System_printf("\n Please press a key ( 0 to 9 or A, B, C, D) \n");
+		System_flush();
+		scanf("%d", &input);
+
+		if(input == 0){
+			freq1 = 941;
+			freq2 = 1335;
+		}else if(input == 1){
+			freq1 = 697;
+			freq2 = 1209;
+		}else if(input == 2){
+			freq1 = 697;
+			freq2 = 1335;
+		}else if(input == 3){
+			freq1 = 697;
+			freq2 = 1477;
+		}else if(input == 4){
+			freq1 = 770;
+			freq2 = 1209;
+		}else if(input == 5){
+			freq1 = 770;
+			freq2 = 1335;
+		}else if(input == 6){
+			freq1 = 770;
+			freq2 = 1477;
+		}else if(input == 7){
+			freq1 = 852;
+			freq2 = 1209;
+		}else if(input == 8){
+			freq1 = 852;
+			freq2 = 1335;
+		}else{
+			freq1 = 852;
+			freq2 = 1477;
+		}
 
 
-
-
+	System_printf("\n Please a wait for few minutes\n");
+	System_flush();
+	System_printf("\n The GTZ is %d\n", gtz_out[0]);
+	a1=0, a2=0, f1=0, f2=0;
 	}
 }
 
